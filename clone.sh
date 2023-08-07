8c25a15f5fcba7ef89114e99cd38fba6ff61d894
krtsato <kuratasatoc@gmail.com>
2023-08-07 12:11:49 +0900

    chore: update clone sh

--- clone.sh
+++ clone.sh
@@ -1,12 +1,24 @@
 #!/bin/bash -eu
 
 ORG=$1
+SSH_HOST="github.com"
 REPOS=( \
-  foo \
-  bar
+  eheya-data-analytics-batch \
+  eheya-data-analytics-cost \
+  eheya-data-analytics-data-api \
+  eheya-data-analytics-data-bridge \
+  eheya-data-analytics-data-management \
+  eheya-data-analytics-secure-data \
+  eheya-data-analytics-tools \
+  eheya-data-monitoring \
+  red-frasco-ad-repo \
+  aws-infra-eheya
 )
 
-for i in "${!REPOS[@]}"
-do
-  git clone "git@github.com:$ORG/${REPOS[$i]}.git"
+if [ "$ORG" = "red-frasco" ]; then
+  SSH_HOST="github.com.red"
+fi
+
+for i in "${!REPOS[@]}"; do
+  git clone "git@$SSH_HOST:$ORG/${REPOS[$i]}.git"
 done
